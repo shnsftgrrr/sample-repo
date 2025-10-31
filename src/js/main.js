34850e60abda2ce6755edbf040ecfba261ffd579
Sheena Tegerero <sheenategerero.upwardnext@gmail.com>
2025-10-31 19:36:55 +0800

    Coding task

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Foundation + SCSS + Pug + JS with live reload!');
+const form = document.getElementById('signup');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  const firstname = document.getElementById('firstname').value.trim();
+  const lastname = document.getElementById('lastname').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+  const confirmPassword = document.getElementById('confirmpassword').value.trim();
+
+  if(password !== confirmPassword) {
+    message.textContent = "Passwords do not match";
+    return;
+  }
+
 });
